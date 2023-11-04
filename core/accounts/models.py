6c19dfab094202f76848c96890715bd89a365cd6
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-04 15:08:24 +0330

    impelement managers.py and a few change in Custom User

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
 from django.utils import timezone
+from .managers import CustomUserManager
 
 
-class MyUser(AbstractBaseUser, PermissionsMixin):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=timezone.now)
     updated_at = models.DateTimeField(auto_now=timezone.now)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["email"]
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
