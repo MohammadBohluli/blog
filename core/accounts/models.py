92c2d9584904fbeb6e8c878450b972986edd186a
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-04 12:31:34 +0330

    implement MyUser class from AbstracteBaseModel

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
+from django.utils import timezone
 
-# Create your models here.
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=timezone.now)
+    updated_at = models.DateTimeField(auto_now=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
+        return self.email
