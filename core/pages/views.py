81d556718208e4767a84c4894fa7afa29b70a104
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-08 19:37:04 +0330

    implement basic home and about pages

--- core/pages/views.py
+++ core/pages/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Home'
+        return context
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'About'
+        return context
