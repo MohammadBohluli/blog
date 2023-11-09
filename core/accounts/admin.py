a6a16f7454bc7bc8e0dfecdf8ad8629d31919720
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-09 16:37:57 +0330

    add last_name and first_name to the user class

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -5,45 +5,60 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    ordering = ('email',)
-    list_display = ("email", "is_staff", "is_active", "is_superuser", "created_at", "updated_at")
+    ordering = ("email",)
+    list_display = (
+        "email",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "created_at",
+        "updated_at",
+    )
     search_fields = ("email",)
 
     fieldsets = (
-        ("Authentications",
-         {"fields":
-              ("email",
-               "password",
-               )}
-         ),
-        ("Permissions",
-         {"fields":
-              ("is_staff",
-               "is_active",
-               "groups",
-               "user_permissions"
-               )}),
-        ("Times and Date",
-         {"fields":
-              ("last_login",
-               )})
+        (
+            "Authentications",
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Informations",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+        ("Times and Date", {"fields": ("last_login",)}),
     )
 
     add_fieldsets = (
-        (None,
-         {"fields":
-              ("email",
-               "password1",
-               "password2",
-               )}
-         ),
-        ("Permissions",
-         {"fields":
-              ("is_staff",
-               "is_active",
-               "groups",
-               "user_permissions"
-               )})
+        (
+            "Informations",
+            {
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
     )
 
 
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
