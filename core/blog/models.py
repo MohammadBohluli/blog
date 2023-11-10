8080b68dc68676e2dc0c274354c2e2ae74abb22b
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-10 18:02:27 +0330

    create model post and category

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from accounts.models import CustomUser
+from django.utils import timezone
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        PUBLISHED = "P", "Publish"
+        DRAFT = "D", "Draft"
+
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, blank=False)
+    category = models.ManyToManyField("Category")
+    content = models.TextField()
+    status = models.CharField(
+        max_length=1,
+        choices=Status.choices,
+        default=Status.DRAFT,
+    )
+
+    published_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
