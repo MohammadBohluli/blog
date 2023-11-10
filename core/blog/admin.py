d485f68276af15b0323b2409ad8bf89bfa40fa38
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-10 18:10:24 +0330

    implements admin's Post model and category model

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post, Category
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = [
+        "author",
+        "title",
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ["name"]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
