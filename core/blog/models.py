838376d36708aa80fb6665da454f373c8683ba3a
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-13 13:24:04 +0330

    Revert "add get_abulate_url to the Post Model"
    This reverts commit 48daffb357f5f06f3d384dc87331c4348778cb75.

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+
+from django.db import models
 from accounts.models import CustomUser
 from django.utils import timezone
-from django.urls import reverse
 
 
 class Post(models.Model):
@@ -25,9 +26,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return self.id
+    
 
 
 class Category(models.Model):
