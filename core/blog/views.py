bb7a8b294560ffd8a680370511940b2004ccb26f
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-20 00:41:39 +0330

    add:template share post

--- core/blog/views.py
+++ core/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Post
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 def post_list_view(request):
 
@@ -40,16 +41,27 @@ def post_detail_view(request, year, month, day, post):
 def post_share_view(request, post_id):
     post = get_object_or_404(Post, id=post_id,status=Post.Status.PUBLISHED)
 
+    # use the sent variable in the template to display 
+    # a success message when the form is successfully submitted
+    is_sent = False
+
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             cd = form.cleaned_data
-        else:
-            form = EmailPostForm()
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f" recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url} {cd['name']}s comment: {cd['comment']}"
+
+            send_mail(subject, message, 'your_account@gmail.com',[cd['to']])
+            is_sent = True
+    else:
+        form = EmailPostForm()
 
     context = {
         'post': post,
         'form': form,
+        'is_sent': is_sent,
     }
 
     return render(request, 'pages/blog/post_share.html', context)
