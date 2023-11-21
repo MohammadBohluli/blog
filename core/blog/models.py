fbde8f3a7e58f652d1cdce9d6637ea0f9112b0e6
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-21 16:06:44 +0330

    feat:create comment model

--- core/blog/models.py
+++ core/blog/models.py
@@ -41,3 +41,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created',]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
