94acf6b2fb19c2df227f99e1b1dfc599914ea582
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-26 19:18:48 +0330

    add: dockerize postgresql

--- core/config/settings.py
+++ core/config/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get('SQL_NAME'),
-        "USER": os.environ.get('SQL_USER'),
-        "PASSWORD": os.environ.get('SQL_PASSWORD'),
-        "HOST": os.environ.get('SQL_HOST', default='localhost'),
-        "PORT": os.environ.get('SQL_PORT',default='5432'),
+        "NAME": os.environ.get("SQL_NAME"),
+        "USER": os.environ.get("SQL_USER"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "HOST": os.environ.get("SQL_HOST"),
+        "PORT": os.environ.get("SQL_PORT"),
     }
 }
 
