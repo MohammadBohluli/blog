dfc1b24ae3be94fc8703e205f7c7aaa88393a44c
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-28 16:50:56 +0330

    feat: register page

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import LoginViewPage, LogoutViewPage
+from .views import LoginViewPage, LogoutViewPage, RegisterViewPage
+
+app_name = 'account'
 urlpatterns = [
     path('login/', LoginViewPage.as_view(), name='login'),
     path('logout/', LogoutViewPage.as_view(), name='logout'),
+    path('register/', RegisterViewPage.as_view(), name='register'),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,7 +1,14 @@
+from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
-
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
 class LoginViewPage(LoginView):
     template_name = 'registration/login_page.html'
 
 class LogoutViewPage(LogoutView):
     template_name = 'registration/logout_page.html'
+
+class RegisterViewPage(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('account:login')
+    template_name = 'registration/register_page.html'
