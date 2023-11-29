1d29103441ff52a2721f43d4d2719d3fcfc149ea
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-11-29 13:17:53 +0330

    refactor: register page CBV to FBV

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import LoginViewPage, LogoutViewPage, RegisterViewPage
+from .views import LoginViewPage, LogoutViewPage, register_view
 
 app_name = 'account'
 urlpatterns = [
     path('login/', LoginViewPage.as_view(), name='login'),
     path('logout/', LogoutViewPage.as_view(), name='logout'),
-    path('register/', RegisterViewPage.as_view(), name='register'),
+    path('register/', register_view, name='register'),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,14 +1,35 @@
-from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 class LoginViewPage(LoginView):
     template_name = 'registration/login_page.html'
 
 class LogoutViewPage(LogoutView):
     template_name = 'registration/logout_page.html'
 
-class RegisterViewPage(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('account:login')
-    template_name = 'registration/register_page.html'
+def register_view(request):
+
+    if request.method == "POST":
+    
+        form = CustomUserCreationForm(request.POST)
+        print('form_register', form)
+
+        if form.is_valid():
+            form.save()
+
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+            
+
+            user = authenticate(email=email, password=password)
+            login(request,user)
+            
+            return redirect('pages:home_page')
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/register_page.html', context)
