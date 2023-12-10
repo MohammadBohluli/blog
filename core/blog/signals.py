0a9dde63f573ea423435dd9a138092ed58d8368f
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2023-12-10 18:01:22 +0330

    add: creat uniq signal for each post

--- core/blog/signals.py
+++ core/blog/signals.py
@@ -5,4 +5,24 @@ from .models import Post
 
 @receiver(signal=pre_save, sender=Post)
 def creat_auto_post_slug(sender, instance, *args, **kwargs):
-    instance.slug = slugify(instance.title, allow_unicode=True)
+    if not instance.slug:
+        instance.slug = creat_uniq_slug(instance)
+
+
+
+def creat_uniq_slug(instance, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title, allow_unicode=True)
+    
+    # instance.__class__ its same Post
+    qs = instance.__class__.objects.filter(slug=slug)
+
+    if qs.exists():
+        new_slug = f"{slug}-{qs.first().id}"
+        return creat_uniq_slug(instance, new_slug)
+    
+    return slug
+
+
